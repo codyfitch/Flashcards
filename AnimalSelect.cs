aefca03cd1d9f636025839f672a459ca4852eb1c
Cody Fitch <35245717+codyfitch@users.noreply.github.com>
2019-10-13 17:32:15 -0600

    Animal initial commit - local server
    Commit with code connecting to local SQL server running on Raspberry Pi.

---
+++ AnimalSelect.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AnimalSelect : MonoBehaviour
+{
+    string duckOn = "https://10.0.0.21:80/DuckOn.php";
+    string duckOff = "https://10.0.0.21:80/DuckOff.php";
+    string lionOn = "https://10.0.0.21:80/LionOn.php";
+    string lionOff = "https://10.0.0.21:80/LionOff.php";
+    string monkeyOn = "https://10.0.0.21:80/MonkeyOn.php";
+    string monkeyOff = "https://10.0.0.21:80/MonkeyOff.php";
+
+    bool duckStatus = false;
+    bool lionStatus = false;
+    bool monkeyStatus = false;
+
+    private void Start()
+    {
+        StartCoroutine(DuckStop());
+        StartCoroutine(LionStop());
+        StartCoroutine(MonkeyStop());
+    }
+    public void DuckClick()
+    {
+        if(duckStatus == false)
+        {
+            StartCoroutine(DuckStart());
+        } else if(duckStatus == true)
+        {
+            StartCoroutine(DuckStop());
+        }
+    }
+
+    public void LionClick()
+    {
+        if(lionStatus == false)
+        {
+            StartCoroutine(LionStart());
+        } else if(lionStatus == true)
+        {
+            StartCoroutine(LionStop());
+        }
+    }
+
+    public void MonkeyClick()
+    {
+        if(monkeyStatus == false)
+        {
+            StartCoroutine(MonkeyStart());
+        } else if(monkeyStatus == true)
+        {
+            StartCoroutine(MonkeyStop());
+        }
+    }
+
+
+    IEnumerator DuckStart()
+    {
+        UnityWebRequest messUpdate = UnityWebRequest.Get(duckOn);
+        yield return messUpdate.SendWebRequest();
+        duckStatus = true;
+    }
+
+    IEnumerator DuckStop()
+    {
+        UnityWebRequest messUpdate = UnityWebRequest.Get(duckOff);
+        yield return messUpdate.SendWebRequest();
+        duckStatus = false;
+    }
+
+    IEnumerator LionStart()
+    {
+        UnityWebRequest messUpdate = UnityWebRequest.Get(lionOn);
+        yield return messUpdate.SendWebRequest();
+        lionStatus = true;
+    }
+
+    IEnumerator LionStop()
+    {
+        UnityWebRequest messUpdate = UnityWebRequest.Get(lionOff);
+        yield return messUpdate.SendWebRequest();
+        lionStatus = false;
+    }
+
+    IEnumerator MonkeyStart()
+    {
+        UnityWebRequest messUpdate = UnityWebRequest.Get(monkeyOn);
+        yield return messUpdate.SendWebRequest();
+        monkeyStatus = true;
+    }
+
+    IEnumerator MonkeyStop()
+    {
+        UnityWebRequest messUpdate = UnityWebRequest.Get(monkeyOff);
+        yield return messUpdate.SendWebRequest();
+        monkeyStatus = false;
+    }
+}
---
+++ AnimalToggle.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class AnimalToggle : MonoBehaviour
+{
+    public GameObject duck;
+    public GameObject lion;
+    public GameObject monkey;
+
+    public Text duckText;
+    public Text lionText;
+    public Text monkeyText;
+
+    //Add sound effects
+
+    //Get SQL data through PHP script for security
+    string messageData = "https://10.0.0.21/messageData.php";
+    public Text incomingText;
+
+    private void Start()
+    {
+        StartCoroutine(GetData());
+    }
+
+
+    private void Update()
+    {
+
+        if (int.Parse(duckText.text) == 1)
+        {
+            duck.SetActive(true);
+        }
+        else
+        {
+            duck.SetActive(false);
+        }
+
+        if (int.Parse(lionText.text) == 1)
+        {
+            lion.SetActive(true);
+        }
+        else
+        {
+            lion.SetActive(false);
+        }
+
+        if (int.Parse(monkeyText.text) == 1)
+        {
+            monkey.SetActive(true);
+        }
+        else
+        {
+            monkey.SetActive(false);
+        }
+    }
+
+    //Coroutine to determine how often to ping the server to prevent overflow
+    IEnumerator DataTimer()
+    {
+        yield return new WaitForSeconds(5.0f);
+        StartCoroutine(GetData());
+    }
+
+    IEnumerator GetData()
+    {
+        //Import data from SQL server through PHP script above
+        UnityWebRequest messUpdate = UnityWebRequest.Get(messageData);
+        yield return messUpdate.SendWebRequest();
+
+        if (messUpdate.error != null)
+        {
+            Debug.Log(messUpdate.error);
+        }
+        else
+        {
+            //Split incoming string into message categories
+            incomingText.text = messUpdate.downloadHandler.text;
+            string allData = incomingText.text;
+            string[] splitStr = allData.Split(" "[0]);
+            int[] value = new int[splitStr.Length - 1];
+            string[] splitArr = allData.Split(char.Parse(";"));
+            duckText.text = splitArr[0];
+            lionText.text = splitArr[1];
+            monkeyText.text = splitArr[2];
+        }
+
+        StartCoroutine(DataTimer());
+    }
+}
